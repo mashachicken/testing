e79003d8c152c8e2140e16ca75b73423ed0e3ff8
George <greg@gregs-MacBook-Pro.local>
2019-11-08 08:30:00 -0800

    add pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+// BACK END LOGIC
+function PizzaPie (size, toppings){
+  this.size = size,
+  this.toppings = toppings,
+  this.price = 0
+};
+
+
+
+
+
+
+// FRONT END LOGIC
 $(document).ready(function(){
 
 });
