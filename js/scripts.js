9dffbc01ea9d48300b867d306e25a852ac2c552b
George <greg@gregs-MacBook-Pro.local>
2019-11-08 12:21:09 -0800

    add js functions

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,34 @@ function PizzaPie (size, toppings){
 
 
 
+
+
+
+
+
+
+
+
 // FRONT END LOGIC
+
+
+
+
 $(document).ready(function(){
 $("#orderMenu").submit(function(e){
   event.preventDefault();
-
   var sizeInput = $("#sizeSelections:checked");
   var myToppings = [];
-
+  var totalToppings = [];
   $("input:checkbox[name=toppings]:checked").each(function() {
     myToppings.push (parseInt($(this).val()));
+  for (var i=0; i<myToppings.length; i++){
+    totalToppings += myToppings[i];
+  };
   });
-  var totalToppingCount = function myToppingsCount (myTopping) {
-  myToppings.for(i = 0; sum = 0; i <= myToppings.length; sum += myToppings[i++])
-  return sum
-};
-
-
   console.log(myToppings);
-  console.log(totalToppingCount);
+  console.log(totalToppings);
+
+
   });
   });
