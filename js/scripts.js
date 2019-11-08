0a9443225154d851bc0117c0941fa5aea10ecc00
George <greg@gregs-MacBook-Pro.local>
2019-11-08 14:41:30 -0800

    getPrice prototype fixed

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,20 @@ function PizzaPie (size, toppings){
   this.price = 0
 };
 
-PizzaPie.prototype.getPrice = function (total){
-  if (totalToppings >= 1){
-    this.price += 3;
+PizzaPie.prototype.getPrice = function (){
+  if (totalToppings >= 1 && totalToppings <3){
+    this.price += 5;
+    console.log(totalToppings);
   }
   if (totalToppings >= 3 && totalToppings < 6){
-    this.price += 6;
-  };
-  if (totalToppings > 6 && totalToppings<=7){
     this.price += 10;
   };
-};
+  if (totalToppings > 6 && totalToppings<=7){
+    this.price += 12;
+  };
+  return this.price;
 
-console.log(this.price);
+  };
 
 
 
@@ -34,11 +35,14 @@ var sizeInput = $("#sizeSelections:checked");
 // UNDEFINED ARRAYS TO CATCH VALUES OF EACH TOPPING SELECTION
 var myToppings = [];
 var totalToppings = 0;
+var toppingsList = [];
+console.log(toppingsList);
 
 // THIS LOOP IS GRABING THE VALUE FOR EACH CHECKED BOX
 function toppingCollector (mytopping) {
 $("input:checkbox[name=toppings]:checked").each(function() {
   myToppings.push (parseInt($(this).val()));
+  toppingsList.push($(this).html());
 });
 };
 
@@ -51,9 +55,11 @@ for (var i=0; i<myToppings.length; i++){
 $(document).ready(function(){
 $("#orderMenu").submit(function(e){
   event.preventDefault();
+  var newPizza = new PizzaPie ();
   toppingCollector();
   toppingSum(myToppings)
-  console.log(myToppings)
-  console.log(totalToppings);
+  var price = newPizza.getPrice();
+  console.log(price);
+  console.log(newPizza);
   });
   });
