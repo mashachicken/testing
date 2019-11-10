ace371a9f97bee7a3133c747e9cd0cda88672f69
George <greg@gregs-MacBook-Pro.local>
2019-11-09 17:11:07 -0800

    clean up html and comments/add css

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // BACK END LOGIC
-function PizzaPie (size, toppings){
-  this.size = size,
-  this.toppings = toppings,
+function PizzaPie(){
+  this.size = ""
+  this.toppings = []
   this.price = 0
 };
 
 // WORKING!✅
-// THIS PROTOTYPE WILL SET THE PIZZA PRICE IN REGARDS TO THE TOTAL TOPPINGS
+// THIS PROTOTYPE WILL SET THE PIZZA PRICE IN REGARDS TO SIZE SELECTION AND WILL ALSO  ADD 1 FOR EACH STRING IN THE TOPPINGS ARRAT (TOTAL PRICE THAT WILL BE DISPLAYED ON DOM)
 PizzaPie.prototype.calculatePrice = function() {
     if (this.size === "Small") {
       this.price += 5
@@ -22,7 +22,7 @@ PizzaPie.prototype.calculatePrice = function() {
     this.price += numberOfToppings
 };
 
-
+// THIS PROTOTYPE WILL SET THE SIZE SELECTION OF THE PIZZA IN REGARDS TO THE SELECTION VALUE (WHAT SIZE WILL BE DISPLAYED ON DOM)
 PizzaPie.prototype.setSize= function(sizeOpt){
   if (sizeOpt === "1"){
     this.size = "Small"
@@ -43,6 +43,7 @@ this.toppings = toppings;
 // WORKING!✅
 // THIS LOOP IS GRABING THE VALUE FOR EACH CHECKED BOX
 function toppingCollector() {
+  // THIS ARRAY WILL HOLD THE SELECTED STRINGS(QTY OF STRINGS WILL BE USED TO SET PRICE/ EACH INDIVIDUAL VALUE SELECTION WILL BE SET AS TOPPINGS KEY VALUE)
   var myToppings = [];
   $("input:checkbox[name=toppings]:checked").each(function() {
     myToppings.push($(this).val());
@@ -50,7 +51,7 @@ function toppingCollector() {
   return myToppings;
 };
 
-
+// THIS FUNCTION WILL SHOW PIZZAPIE VALUES ON DOM
 function showPizzaDetails (pizzaPie){
   $(".pizza-size").html(pizzaPie.size)
   $(".pizza-toppings").html(pizzaPie.toppings.join(", "));
@@ -62,11 +63,16 @@ function showPizzaDetails (pizzaPie){
 $(document).ready(function(){
   $("#orderMenu").submit(function(e){
     event.preventDefault();
+
     var newPizza = new PizzaPie();
 
     var sizeOpt = $("#sizeSelections").val();
-    newPizza.setSize(sizeOpt);
 
+    newPizza.setSize(sizeOpt);
+    if (newPizza.size ===""){
+    alert("Please select a size!")
+    return
+    };
     var toppings = toppingCollector();
     newPizza.setToppings(toppings);
     newPizza.calculatePrice();
